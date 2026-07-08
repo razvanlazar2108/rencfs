cb3572e3dedd52ed57d73006a8af05ebef298358
Lazar Razvan <grazvan.lazar@gmail.com>
2026-07-08 08:18:50 +0300

    feat: definit structura de date IpfsCipher cu campurile pentru cheie si algoritm

--- src/ipfs_plugin.rs
+++ src/ipfs_plugin.rs
@@ -2,3 +2,8 @@ use crate::crypto::{self, Cipher};
 use crate::crypto::write::CryptoWrite; // Rezolvă eroarea cu .finish()
 use shush_rs::SecretVec;
 use std::io::{Read, Write, Cursor};
+
+pub struct IpfsCipher {
+    key: SecretVec<u8>,
+    cipher: Cipher,
+}
